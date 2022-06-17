dd990190587b20363f2f3c105faf563b224c77cd
ChanghyeonKim93 <hyun91015@gmail.com>
2022-06-17 11:14:55 +0900

    init

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <ros/ros.h>
+
+#include <iostream>
+
+int main(int argc, char** argv) {
+  ros::init(argc, argv, "eskf_node");
+  ros::NodeHandle nh("~");
+
+  try {
+
+  } 
+  catch (const std::exception& e) 
+  {
+
+  }
+}
